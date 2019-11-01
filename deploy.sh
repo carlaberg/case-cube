82acc7ad1bea1b92f0cecc7229cee89bf5c7a731
carlaberg <mail.carl.aberg@gmail.com>
2019-11-01 18:49:20 +0100

    Added bappelsin

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 touch bulle.txt \
-&& touch banan.txt
+&& touch banan.txt \
+&& touch bappelsin.txt
 # echo ‘post-receive: Triggered.’
 # echo ‘post-receive: pulling from repo…’
 # git pull \
