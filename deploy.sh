b28c661a8d39ee64f4050a7483eff116ddf6086d
carlaberg <mail.carl.aberg@gmail.com>
2019-11-01 18:29:11 +0100

    Testing deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo ‘post-receive: Triggered.’
-echo ‘post-receive: pulling from repo…’
-git pull \
-&& echo ‘Done pulling from repo’
+touch bulle.txt \
+&& touch banan.txt
+# echo ‘post-receive: Triggered.’
+# echo ‘post-receive: pulling from repo…’
+# git pull \
+# && echo ‘Done pulling from repo’
 
 #bulle
