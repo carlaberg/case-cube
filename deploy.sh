0cd5b6e5ef6490962b21831be636827d9f43e40f
carlaberg <mail.carl.aberg@gmail.com>
2019-11-02 19:44:40 +0100

    Testing to move index

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-npm install
+npm install \
+&& npm run build \
+&& cp index.html /var/www/case-cube.carlaberg.se/public_html/
 # && pm2 restart caseCubeApi
 # echo ‘post-receive: Triggered.’
 # echo ‘post-receive: pulling from repo…’
