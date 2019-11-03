da68d0b1c51118a246ca214486a66884ff75ea7d
carlaberg <mail.carl.aberg@gmail.com>
2019-11-03 22:22:25 +0100

    aqua

--- app/js/components/public-components/base-layout/Header/styles.js
+++ app/js/components/public-components/base-layout/Header/styles.js
@@ -10,7 +10,7 @@ export const Wrapper = styled(animated.header)`
   z-index: 1;
   flex-shrink: 0;
   letter-spacing: 1.4px;
-  background: deeppink;
+  background: aqua;
   
   svg {
     width: 100%;
