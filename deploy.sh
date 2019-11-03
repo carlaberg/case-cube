32725637be4d3091c911694f8dd4fdf86f49018e
carlaberg <mail.carl.aberg@gmail.com>
2019-11-03 23:23:47 +0100

    Added comments to deploy process

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-npm install \
+echo 'Installing dependencies' \
+&& npm install \
+&& echo 'Done installing dependencies' \
+&& echo 'Prepearing production build' \
 && npm run build \
+&& echo 'Done prepearing production build' \
+&& echo 'Cleaning public folder' \
 && rm -rf /var/www/case-cube.carlaberg.se/public_html/* \
+&& echo 'Done cleaning public folder' \
+&& echo 'Copying new production build to public folder' \
 && cp -r public/. /var/www/case-cube.carlaberg.se/public_html/ \
-&& pm2 restart caseCubeApi
-# echo ‘post-receive: Triggered.’
-# echo ‘post-receive: pulling from repo…’
-# git pull \
-# && echo ‘Done pulling from repo’
-
-#bulle
+&& echo 'Done copying new production build to public folder' \
+&& echo 'Restarting node server' \
+&& pm2 restart caseCubeApi \
+&& echo 'Done restarting node server' \
+&& echo 'Deploy done'
